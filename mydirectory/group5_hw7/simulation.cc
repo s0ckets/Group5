40b449b11062b1610519c528ac0abdce05e8c42f
Russell Burckhalter <Burckhar@email.sc.edu>
2016-11-29 21:03:16 -0500

    Formatted File
    Formatted the file with spacing & based on the Google Style Guide

--- mydirectory/group5_hw7/simulation.cc
+++ mydirectory/group5_hw7/simulation.cc
@@ -20,15 +20,13 @@ static const string kTag = "SIM: ";
 /****************************************************************
 * Constructor.
 **/
-Simulation::Simulation()
-{
+Simulation::Simulation(){
 }
 
 /****************************************************************
 * Destructor.
 **/
-Simulation::~Simulation()
-{
+Simulation::~Simulation(){
 }
 
 /****************************************************************
@@ -39,61 +37,64 @@ Simulation::~Simulation()
 * General functions.
 **/
 void Simulation::ReadPrecincts(Scanner& infile) {
-while (infile.HasNext()) {
-OnePct new_pct;
-new_pct.ReadData(infile);
-pcts_[new_pct.GetPctNumber()] = new_pct;
-} // while (infile.HasNext()) {
+  while (infile.HasNext()) {
+    
+    OnePct new_pct;
+    new_pct.ReadData(infile);
+    pcts_[new_pct.GetPctNumber()] = new_pct;
+  } // while (infile.HasNext()) {
 } // void Simulation::ReadPrecincts(Scanner& infile) {
-
 /****************************************************************
 **/
 void Simulation::RunSimulation(const Configuration& config,
-MyRandom& random, ofstream& out_stream)
-{
-string outstring = "XX";
-int pct_count_this_batch = 0;
-for(auto iterPct = pcts_.begin(); iterPct != pcts_.end(); ++iterPct)
-{
-OnePct pct = iterPct->second;
+                   MyRandom& random, ofstream& out_stream){
+  string outstring = "XX";
+  int pct_count_this_batch = 0;
+  for(auto iterPct = pcts_.begin(); iterPct != pcts_.end();
+      ++iterPct){
 
-int expected_voters = pct.GetExpectedVoters();
-if ((expected_voters <=  config.min_expected_to_simulate_) ||
-(expected_voters >   config.max_expected_to_simulate_)) continue;
+    OnePct pct = iterPct->second;
+    int expected_voters = pct.GetExpectedVoters();
+    if ((expected_voters <= config.min_expected_to_simulate_) ||
+        (expected_voters > config.max_expected_to_simulate_)){
+    
+      continue;
+    }
 
-outstring = kTag + "RunSimulation for pct " + "\n";
-outstring += kTag + pct.ToString() + "\n";
-Utils::Output(outstring, out_stream, Utils::log_stream);
+    outstring = kTag + "RunSimulation for pct " + "\n";
+    outstring += kTag + pct.ToString() + "\n";
+    Utils::Output(outstring, out_stream, Utils::log_stream);
 
-++pct_count_this_batch;
-pct.RunSimulationPct(config, random, out_stream);
+    ++pct_count_this_batch;
+    pct.RunSimulationPct(config, random, out_stream);
 
-//    break; // we only run one pct right now
-} // for(auto iterPct = pcts_.begin(); iterPct != pcts_.end(); ++iterPct)
+    //    break; // we only run one pct right now
+  } // for(auto iterPct = pcts_.begin(); iterPct != pcts_.end(); ++iterPct)
 
-outstring = kTag + "PRECINCT COUNT THIS BATCH "
-+ Utils::Format(pct_count_this_batch, 4) + "\n";
-//  Utils::Output(outstring, out_stream);
-Utils::Output(outstring, out_stream, Utils::log_stream);
-//  out_stream << outstring << endl;
-//  out_stream.flush();
-//  Utils::log_stream << outstring << endl;
-//  Utils::log_stream.flush();
+  outstring = kTag + "PRECINCT COUNT THIS BATCH "
+  + Utils::Format(pct_count_this_batch, 4) + "\n";
+  //  Utils::Output(outstring, out_stream);
+  Utils::Output(outstring, out_stream, Utils::log_stream);
+  //  out_stream << outstring << endl;
+  //  out_stream.flush();
+  //  Utils::log_stream << outstring << endl;
+  //  Utils::log_stream.flush();
 
 } // void Simulation::RunSimulation()
 
 /****************************************************************
 * Usual 'ToString'.
 **/
-string Simulation::ToString()
-{
-string s = "";
+string Simulation::ToString(){
 
-for(auto iterPct = pcts_.begin(); iterPct != pcts_.end(); ++iterPct)
-{
-s += kTag + (iterPct->second).ToString() + "\n";
-}
+  string s = "";
 
-return s;
+  for(auto iterPct = pcts_.begin(); iterPct != pcts_.end(); 
+         ++iterPct){
+
+    s += kTag + (iterPct->second).ToString() + "\n";
+  }
+
+  return s;
 } // string Simulation::ToString()
 
